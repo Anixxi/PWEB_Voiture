98c36bc298fca6d9ce81efe8a0f5964d5d960a9f
Anixxi <79166272+Anixxi@users.noreply.github.com>
2021-10-08 18:23:08 +0200

    Add files via upload

---
+++ modele/location.sql
@@ -0,0 +1,127 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1
+-- http://www.phpmyadmin.net
+--
+-- Client :  localhost
+-- Généré le :  Ven 08 Octobre 2021 à 15:52
+-- Version du serveur :  5.7.11
+-- Version de PHP :  5.6.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `location`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `client`
+--
+
+CREATE TABLE `client` (
+  `id` int(11) NOT NULL,
+  `nom` varchar(20) NOT NULL,
+  `pseudo` varchar(20) NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `mdp` varchar(20) NOT NULL,
+  `nomE` varchar(20) NOT NULL,
+  `adresseE` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `client`
+--
+
+INSERT INTO `client` (`id`, `nom`, `pseudo`, `email`, `mdp`, `nomE`, `adresseE`) VALUES
+(1, 'LeBlanc', 'LB', 'fafge@gmail.com', '2002', 'Microsoft', '18 rue victor hugo');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `facture`
+--
+
+CREATE TABLE `facture` (
+  `id` int(11) NOT NULL,
+  `ide` int(11) NOT NULL,
+  `idv` int(11) NOT NULL,
+  `dateD` date NOT NULL,
+  `dateF` date NOT NULL,
+  `valeur` double NOT NULL,
+  `etatR` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `vehicule`
+--
+
+CREATE TABLE `vehicule` (
+  `id` int(11) NOT NULL,
+  `type` int(11) NOT NULL,
+  `nb` int(11) NOT NULL,
+  `caract` varchar(50) NOT NULL,
+  `photo` varchar(50) NOT NULL,
+  `etatL` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `vehicule`
+--
+
+INSERT INTO `vehicule` (`id`, `type`, `nb`, `caract`, `photo`, `etatL`) VALUES
+(1, 306, 1, 'automatique', 'peugot306', 'disponible');
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `client`
+--
+ALTER TABLE `client`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `facture`
+--
+ALTER TABLE `facture`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `vehicule`
+--
+ALTER TABLE `vehicule`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `client`
+--
+ALTER TABLE `client`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT pour la table `facture`
+--
+ALTER TABLE `facture`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `vehicule`
+--
+ALTER TABLE `vehicule`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
